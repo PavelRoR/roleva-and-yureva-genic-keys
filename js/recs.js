1e1cfb93c1e3833f086ba307fdc8855d0a7823ce
pavelp21 <pavelvp2182@yandex.ru>
2018-11-03 11:39:38 +0500

    Recs big screens are done

--- js/recs.js
+++ js/recs.js
@@ -10,58 +10,67 @@ $(document).ready(function () {
             }, 1500)
         });
     });
-    /* Таймер */
-    var clock;
-    var futureDate = new Date("September 28, 2018 00:00 AM UTC+3");
-    var currentDate = new Date();
-    var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+    $(function () {
+        /* Таймер */
+        var clock;
+        var futureDate = new Date("November 1, 2018 00:00 AM UTC+3");
+        var currentDate = new Date();
+        var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
-    function dayDiff(first, second) {
-        return (second - first) / (1000 * 60 * 60 * 24);
-    }
-    if (dayDiff(currentDate, futureDate) < 100) {
-        $('.clock').addClass('twoDayDigits');
-    } else {
-        $('.clock').addClass('threeDayDigits');
-    }
-    if (diff < 0) {
-        diff = 0;
-        $('.clock-stop').addClass("alert-danger");
-        $('.clock-stop').html("Скидка уменьшилась!");
-    }
-    clock = $('.clock').FlipClock(diff, {
-        clockFace: 'HourlyCounter',
-        countdown: true,
-        language: 'ru',
-        callbacks: {
-            stop: function () {
-                $('.clock-stop').addClass("alert-danger");
-                $('.clock-stop').html("Скидка уменьшилась!");
-            }
-        },
+        function dayDiff(first, second) {
+            return (second - first) / (1000 * 60 * 60 * 24);
+        }
+        if (dayDiff(currentDate, futureDate) < 100) {
+            $('.clock').addClass('twoDayDigits');
+        } else {
+            $('.clock').addClass('threeDayDigits');
+        }
+        if (diff < 0) {
+            diff = 0;
+            $('.clock-stop').addClass("alert-danger");
+            $('.clock-stop').html("Скидка уменьшилась!");
+            // $('#sale_1').text('1 000р');
+            // $('#sale_2').text('2 000р');
+            // $('#today_1').text('10 400р.');
+            // $('#today_2').text('15 900р.');
+            // $('#link_1').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2320&clean=true&lg=ru');
+            // $('#link_2').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2324&clean=true&lg=ru');
+        }
+        clock = $('.clock').FlipClock(diff, {
+            clockFace: 'HourlyCounter',
+            countdown: true,
+            language: 'ru',
+            callbacks: {
+                stop: function () {
+                    $('.clock-stop').addClass("alert-danger");
+                    $('.clock-stop').html("Скидка уменьшилась!");
+                    // $('#sale_1').text('1 000р');
+                    // $('#sale_2').text('2 000р');
+                    // $('#today_1').text('10 400р.');
+                    // $('#today_2').text('15 900р.');
+                    // $('#link_1').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2320&clean=true&lg=ru');
+                    // $('#link_2').attr('href', 'https://aleksandrkudryashov1.autoweboffice.ru/?r=ordering/cart/as1&id=2324&clean=true&lg=ru');
+                }
+            },
+        });
     });
     /*Записи*/
     (function ($) {
         jQuery.fn.lightTabs = function (options) {
-
             var createTabs = function () {
                 tabs = this;
                 i = 0;
-
                 showPage = function (i) {
                     $(tabs).children("div").children("div").hide();
                     $(tabs).children("div").children("div").eq(i).show();
                     $(tabs).children("ul").children("li").removeClass("active");
                     $(tabs).children("ul").children("li").eq(i).addClass("active");
                 }
-
                 showPage(0);
-
                 $(tabs).children("ul").children("li").each(function (index, element) {
                     $(element).attr("data-page", i);
                     i++;
                 });
-
                 $(tabs).children("ul").children("li").click(function () {
                     showPage(parseInt($(this).attr("data-page")));
                 });
